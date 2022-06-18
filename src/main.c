680b0c9201871aad64a06ee113b8c54b646a1420
harvey.konan <harveykonan@gmail.com>
2022-06-18 22:34:28 +0200

    Fix : ajout des fractales

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+#define NBRE_ITERATION_MAX 15000
 
 void SDL_ExitWithError(const char* message);
 
@@ -9,6 +11,37 @@ int main(int argc, char** argv) {
 
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
+    float c_x1 = 0.0;
+    float c_y1 = 0.0; 
+    float c_x2 = 0.0;
+    float c_y2 = 0.0;
+    float image_x = 0.0;
+    float image_y = 0.0;
+    int zoom = 100;
+    int iter = 0;
+
+    printf("[-----| Fractales |-----]\n");
+    printf("|----- Donnez une constante (z = a + bi) : \n");
+    printf("|----- Donnez un x1 (re) : \n");
+    scanf("%f", &c_x1);
+    printf("|----- Donnez un y1 (im) : \n");
+    scanf("%f", &c_y1);
+    printf("|----- Donnez un x2 (re) : \n");
+    scanf("%f", &c_x2);
+    printf("|----- Donnez un y2 (im) : \n");
+    scanf("%f", &c_y2);
+    printf("|----- Donnez un nombre d'itération : \n");
+    scanf("%d", &iter);
+
+    //Définir les coordonnées de l'images à dessiner à dessiner.
+    image_x = (c_x1 - c_x2) * zoom;
+    image_y = (c_y1 - c_y2) * zoom;
+
+    //Si le nombre d'itération max est inférieur au nombre d'itération.
+    if(iter > NBRE_ITERATION_MAX) {
+        printf("Le nombre d'itération est très grand ! \n");
+        exit(EXIT_FAILURE);
+    }
 
     //Création d'une fenêtre via SDL.
     if(SDL_Init(SDL_INIT_VIDEO) != 0) 
@@ -16,56 +49,78 @@ int main(int argc, char** argv) {
         SDL_ExitWithError("Impossible d'initialiser une fenêtre avec la SDL !");
     }
 
+    window =  SDL_CreateWindow("Fractales Application", 50, 50, 800, 500, SDL_WINDOW_SHOWN);
+
     //Création d'une fenêtre avec rendu.
-    if(SDL_CreateWindowAndRenderer(600, 400, 0, &window, &renderer) != 0) {
+    if(window == NULL) {
+        SDL_ExitWithError("Impossible de créer la fenêtre !");
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_SOFTWARE);
+
+    //Création d'une fenêtre avec rendu.
+    if(renderer == NULL) {
         SDL_ExitWithError("Impossible de créer le rendu de cette fenêtre !");
     }
 
-    if(SDL_SetRenderDrawColor(renderer, 112, 168, 237, SDL_ALPHA_OPAQUE) != 0) {
-        SDL_ExitWithError("Impossible de déssiner un point !");
+
+    SDL_bool programme_lance = SDL_TRUE;
+
+    while (programme_lance)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            switch(event.type) {
+                case SDL_QUIT:
+                    programme_lance = SDL_FALSE;
+                    break;
+                case SDL_KEYDOWN:
+                    if(event.key.keysym.sym == SDLK_q) {
+                        printf("Vous avez clique sur la touche %s", SDLK_q);
+                        programme_lance = SDL_FALSE;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+
+        //Code de la fractale.
+        for (int x = 0; x < image_x; x++)
+        {
+            for (int y = 0; y < image_y; y++)
+            {
+                float c_r = x / zoom + c_x1;
+                float c_i = y / zoom + c_y1;
+                float z_r = 0.0;
+                float z_i = 0.0;
+                int i = 0;
+
+                do
+                {
+                    float tmp = z_r;
+                    z_r = z_r * z_r + z_i * z_i + c_r;
+                    z_i = 2 * z_i * tmp + c_i;
+                    i++;
+                } while (z_r * z_r + z_i * z_i < 4 && i < iter);
+                
+                if(i == iter) {
+                    //On redessine le point sur l'image.
+                    SDL_RenderDrawPoint(renderer, x, y);
+                    SDL_RenderPresent(renderer);
+                }
+            }          
+        }
+        
     }
-
-
-    SDL_Rect rectangle;
-    rectangle.x = 300;
-    rectangle.y = 300;
-    rectangle.h = 200;
-    rectangle.w = 400;
-
-
-    if(SDL_RenderFillRect(renderer, &rectangle) != 0) {
-        SDL_ExitWithError("Impossible de déssiner un rectangle !");
-    }
-
-    if(SDL_SetRenderDrawColor(renderer, 255, 25, 25, SDL_ALPHA_OPAQUE) != 0) {
-        SDL_ExitWithError("Impossible de changer la couleur du rendu !");
-    }
-
-    if(SDL_RenderDrawLine(renderer, 50, 50, 200, 200) != 0) {
-        SDL_ExitWithError("Impossible de dessiner une ligne !");
-    }
-
-    SDL_RenderPresent(renderer);
-    SDL_Delay(10000);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
-    //SDL_Surface *image = NULL;
-    //SDL_Texture *texture = NULL;
-    //window = SDL_CreateWindow("Fractales", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, 0);
-
-    /*if(window == NULL) {
-        SDL_ExitWithError("Impossible de dessiner une ligne !");
-        SDL_Log("ERREUR : Création fenètre échouée > %s\n", SDL_GetError());
-        exit(EXIT_FAILURE); 
-    }*/
-    
-    /*SDL_version nb;
-    SDL_VERSION(&nb);
-
-    printf("Version de SDL : --> %d.%d.%d ! \n", nb.major, nb.minor, nb.patch);*/
     return EXIT_SUCCESS;
 } 
 
